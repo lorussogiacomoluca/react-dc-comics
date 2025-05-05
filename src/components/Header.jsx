0c12e09d308d932f7f5c975783b0a3a1f88a0a2e
lorussogiacomoluca <lorussogiacomoluca@gmail.com>
2025-05-05 18:48:38 +0200

    [CREATE] Setup Header component with imported Logo.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,49 @@
+import Logo from "./shared/Logo.jsx";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="container">
+        <div className="logo">
+          <Logo />
+        </div>
+        <div className="menu">
+          <ul>
+            <li>
+              <a href="#">Characters</a>
+            </li>
+            <li>
+              <a href="#">Comics</a>
+            </li>
+            <li>
+              <a href="#">Movies</a>
+            </li>
+            <li>
+              <a href="#">TV</a>
+            </li>
+            <li>
+              <a href="#">Games</a>
+            </li>
+            <li>
+              <a href="#">Collectibles</a>
+            </li>
+            <li>
+              <a href="#">Videos</a>
+            </li>
+            <li>
+              <a href="#">Fans</a>
+            </li>
+            <li>
+              <a href="#">News</a>
+            </li>
+            <li>
+              <a href="#">Shop</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/shared/Logo.jsx
@@ -0,0 +1,7 @@
+import dcLogo from "../../assets/img/dc-logo.png";
+
+const Logo = () => {
+  return <img src={dcLogo} alt="DC Logo" />;
+};
+
+export default Logo;
