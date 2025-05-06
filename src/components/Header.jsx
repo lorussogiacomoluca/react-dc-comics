a3649eb27a7208a0247340ba2feeb3b59efe18d9
lorussogiacomoluca <lorussogiacomoluca@gmail.com>
2025-05-06 14:50:08 +0200

    [FIX] Finalize hover behavior with stretch using flexbox

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,47 +3,47 @@ import Logo from "./shared/Logo.jsx";
 const Header = () => {
   return (
     <header>
-      <div className="container">
-        <div className="logo">
-          <Logo />
+      <section id="header">
+        <div className="container">
+          <div className="logo">
+            <Logo />
+          </div>
+          <div className="menu">
+            <ul>
+              <li>
+                <a href="#">Characters</a>
+              </li>
+              <li className="active">
+                <a href="#">Comics</a>
+              </li>
+              <li>
+                <a href="#">Movies</a>
+              </li>
+              <li>
+                <a href="#">TV</a>
+              </li>
+              <li>
+                <a href="#">Games</a>
+              </li>
+              <li>
+                <a href="#">Collectibles</a>
+              </li>
+              <li>
+                <a href="#">Videos</a>
+              </li>
+              <li>
+                <a href="#">Fans</a>
+              </li>
+              <li>
+                <a href="#">News</a>
+              </li>
+              <li>
+                <a href="#">Shop</a>
+              </li>
+            </ul>
+          </div>
         </div>
-        <div className="menu">
-          <ul>
-            <li>
-              <a href="#">Characters</a>
-            </li>
-            <li>
-              <a href="#" className="active">
-                Comics
-              </a>
-            </li>
-            <li>
-              <a href="#">Movies</a>
-            </li>
-            <li>
-              <a href="#">TV</a>
-            </li>
-            <li>
-              <a href="#">Games</a>
-            </li>
-            <li>
-              <a href="#">Collectibles</a>
-            </li>
-            <li>
-              <a href="#">Videos</a>
-            </li>
-            <li>
-              <a href="#">Fans</a>
-            </li>
-            <li>
-              <a href="#">News</a>
-            </li>
-            <li>
-              <a href="#">Shop</a>
-            </li>
-          </ul>
-        </div>
-      </div>
+      </section>
     </header>
   );
 };
