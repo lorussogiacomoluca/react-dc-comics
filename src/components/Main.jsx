97c344bf53e303435b13f8f1f251db59db27c4ba
lorussogiacomoluca <lorussogiacomoluca@gmail.com>
2025-05-06 14:55:29 +0200

    [UPD] Add main content area layout

---
+++ src/components/Main.jsx
@@ -0,0 +1,14 @@
+const Main = () => {
+  return (
+    <main>
+      <section id="main-content">
+        <div className="container">
+          <h1>--&gt; Content goes here &lt;--</h1>
+        </div>
+      </section>
+      <section id="main-icon"></section>
+    </main>
+  );
+};
+
+export default Main;
