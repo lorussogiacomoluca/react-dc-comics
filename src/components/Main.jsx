191fd23fdcaf1f076b3455c0cb61a188575c3df7
lorussogiacomoluca <lorussogiacomoluca@gmail.com>
2025-05-06 15:10:50 +0200

    [UPD] finalize card layout in main section

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,3 +1,9 @@
+import first from "../assets/img/buy-comics-digital-comics.png";
+import second from "../assets/img/buy-comics-merchandise.png";
+import third from "../assets/img/buy-comics-subscriptions.png";
+import fourth from "../assets/img/buy-comics-shop-locator.png";
+import fifth from "../assets/img/buy-dc-power-visa.svg";
+
 const Main = () => {
   return (
     <main>
@@ -6,7 +12,40 @@ const Main = () => {
           <h1>--&gt; Content goes here &lt;--</h1>
         </div>
       </section>
-      <section id="main-icon"></section>
+      <section id="main-icon">
+        <div className="container">
+          <div className="card">
+            <div className="card-img">
+              <img src={first} alt="" />
+            </div>
+            <div className="card-text">digital comics</div>
+          </div>
+          <div className="card">
+            <div className="card-img">
+              <img src={second} alt="" />
+            </div>
+            <div className="card-text">digital comics</div>
+          </div>
+          <div className="card">
+            <div className="card-img">
+              <img src={third} alt="" />
+            </div>
+            <div className="card-text">digital comics</div>
+          </div>
+          <div className="card">
+            <div className="card-img">
+              <img src={fourth} alt="" />
+            </div>
+            <div className="card-text">digital comics</div>
+          </div>
+          <div className="card">
+            <div className="card-img">
+              <img src={fifth} alt="DC Power Visa" />
+            </div>
+            <div className="card-text">digital comics</div>
+          </div>
+        </div>
+      </section>
     </main>
   );
 };
