e27243f3d8afd881cc03febb287319314bbd5f01
lorussogiacomoluca <lorussogiacomoluca@gmail.com>
2025-05-06 15:59:17 +0200

    [BONUS] include MainIcon section with icons in Main component

---
+++ src/components/MainIcons.jsx
@@ -0,0 +1,46 @@
+import first from "../assets/img/buy-comics-digital-comics.png";
+import second from "../assets/img/buy-comics-merchandise.png";
+import third from "../assets/img/buy-comics-subscriptions.png";
+import fourth from "../assets/img/buy-comics-shop-locator.png";
+import fifth from "../assets/img/buy-dc-power-visa.svg";
+
+const MainIcons = () => {
+  return (
+    <section id="main-icon">
+      <div className="container">
+        <div className="card">
+          <div className="card-img">
+            <img src={first} alt="" />
+          </div>
+          <div className="card-text">digital comics</div>
+        </div>
+        <div className="card">
+          <div className="card-img">
+            <img src={second} alt="" />
+          </div>
+          <div className="card-text">digital comics</div>
+        </div>
+        <div className="card">
+          <div className="card-img">
+            <img src={third} alt="" />
+          </div>
+          <div className="card-text">digital comics</div>
+        </div>
+        <div className="card">
+          <div className="card-img">
+            <img src={fourth} alt="" />
+          </div>
+          <div className="card-text">digital comics</div>
+        </div>
+        <div className="card">
+          <div className="card-img">
+            <img src={fifth} alt="DC Power Visa" />
+          </div>
+          <div className="card-text">digital comics</div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default MainIcons;
