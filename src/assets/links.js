12ea5592b53c098e8f603b0901122dd3f30ca7d6
lorussogiacomoluca <lorussogiacomoluca@gmail.com>
2025-05-07 17:37:47 +0200

    [BONUS] Uploaded file containing array of objects for navbar links

---
+++ src/assets/links.js
@@ -0,0 +1,63 @@
+const links = [
+    {
+      id: 1,
+      href: "#",
+      label: "Characters",
+      current: false,
+    },
+    {
+      id: 2,
+      href: "#",
+      label: "Comics",
+      current: true,
+    },
+    {
+      id: 3,
+      href: "#",
+      label: "Movies",
+      current: false,
+    },
+    {
+      id: 4,
+      href: "#",
+      label: "TV",
+      current: false,
+    },
+    {
+        id: 4,
+        href: "#",
+        label: "Games",
+        current: false,
+      },
+      {
+        id: 4,
+        href: "#",
+        label: "Collectibles",
+        current: false,
+      },
+      {
+        id: 4,
+        href: "#",
+        label: "Videos",
+        current: false,
+      },
+      {
+        id: 4,
+        href: "#",
+        label: "Fans",
+        current: false,
+      },
+      {
+        id: 4,
+        href: "#",
+        label: "News",
+        current: false,
+      },
+      {
+        id: 4,
+        href: "#",
+        label: "Shop",
+        current: false,
+      }
+    ]
+    
