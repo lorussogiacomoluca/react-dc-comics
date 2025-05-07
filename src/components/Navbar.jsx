f95b7cbe7c90edcd53109295cd4e57905b914c40
lorussogiacomoluca <lorussogiacomoluca@gmail.com>
2025-05-07 17:54:57 +0200

    [FINAL] Rendered navbar menu dynamically via iteration over links array

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,38 +10,7 @@ const Navbar = (props) => {
 
   return (
     <div className="menu">
-      <ul>
-        <li>
-          <a href="#">Characters</a>
-        </li>
-        <li className="active">
-          <a href="#">Comics</a>
-        </li>
-        <li>
-          <a href="#">Movies</a>
-        </li>
-        <li>
-          <a href="#">TV</a>
-        </li>
-        <li>
-          <a href="#">Games</a>
-        </li>
-        <li>
-          <a href="#">Collectibles</a>
-        </li>
-        <li>
-          <a href="#">Videos</a>
-        </li>
-        <li>
-          <a href="#">Fans</a>
-        </li>
-        <li>
-          <a href="#">News</a>
-        </li>
-        <li>
-          <a href="#">Shop</a>
-        </li>
-      </ul>
+      <ul>{navbarMenu}</ul>
     </div>
   );
 };
