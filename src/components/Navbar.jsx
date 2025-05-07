068cc7c4db5a2509383b2d93b4359b8555d3dc0d
lorussogiacomoluca <lorussogiacomoluca@gmail.com>
2025-05-07 17:52:29 +0200

    [UPD] Generated variable to render all menu link elements in Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 
 const Navbar = (props) => {
+  //Variable for generating and rendering list of
+  const navbarMenu = props.links.map((link) => (
+    <li key={`link-${link.id}`} className={link.current ? "active" : ""}>
+      <a href={link.href}>{link.label}</a>
+    </li>
+  ));
+
   return (
     <div className="menu">
       <ul>
